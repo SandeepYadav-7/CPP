72966459aee30a4f4dafe59f32af05c666dead20
Sandeep <sandeepyadav91577@gmail.com>
2024-07-22 13:41:02 +0530

    initial commit

---
+++ variable.cpp
@@ -0,0 +1,17 @@
+/* cpp was created in 1979  
+ * fast program, more control over systen, memory management, higb performance 
+ * major versions: 11, 14, 17 
+ *
+ * Low level: hardware ke nazdik
+ * high level: hardware se dur, logical sounding
+ */
+
+#include <iostream>
+using namespace std;
+
+// Entry point is called by OS, returns an integer
+int main() {
+  // std::cout << ""   // Scope Redolving operator 
+  cout << "Hello World";
+  return 0;
+}
