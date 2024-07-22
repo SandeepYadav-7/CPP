0c06eb54bf6a18dc7dafe43b5e30f54199f28f29
Sandeep <sandeepyadav91577@gmail.com>
2024-07-23 00:01:21 +0530

    variables

--- variable.cpp
+++ variable.cpp
@@ -12,6 +12,23 @@ using namespace std;
 // Entry point is called by OS, returns an integer
 int main() {
   // std::cout << ""   // Scope Redolving operator 
-  cout << "Hello World";
+  
+  /* Variables:
+   * local 
+   * global
+   *
+   */
+
+  /* Data types: 
+   * Built-in      - int, float, double, char, boolean
+   * user-defined  - struct, union, enum 
+   * derived       - array, function, pointer
+   *
+   */
+  int a=14, b=15;
+  float pi=3.14;
+  cout << "Hello World\n";
+  cout << "The value of a is: " <<a << "  The value of be is: " <<b;
+  cout << "\nThe value of pi is: " <<pi;
   return 0;
 }
